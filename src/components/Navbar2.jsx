368b955f81a27a728381420e3d5b493a5c1fe381
Charles <charlesgiftangila@gmail.com>
2025-01-07 13:25:23 +0300

    navbar

--- src/components/Navbar2.jsx
+++ src/components/Navbar2.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 export default function Header() {
   return (
     <header className="fixed top-0 w-full bg-primary/90 backdrop-blur-sm z-50">
-      <div className="container mx-auto px-6 py-4 flex justify-between items-center">
+      <div className="container mx-auto px-1 py-4 flex justify-between ">
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
@@ -16,26 +16,26 @@ export default function Header() {
             </Link>        </motion.div>
         
         <nav className="flex items-center gap-6">
-           <Link to="/" className="text-textPrimary hover:text-secondary transition-colors">
+           <Link to="/" className="text-textPrimary hover:text-white transition-colors">
                        Home
                      </Link>
                      
                      
-                     <Link to="/benefits" className="text-textPrimary hover:text-secondary transition-colors">
+                     <Link to="/benefits" className="text-textPrimary hover:text-white transition-colors">
                        Benefits
                      </Link>
-                     <Link to="/programs" className="text-textPrimary hover:text-secondary transition-colors">
+                     <Link to="/programs" className="text-textPrimary hover:text-white transition-colors">
                        Programs
                      </Link>
-                     {/* <Link to="/contact" className="text-textPrimary hover:text-secondary transition-colors">
+                     {/* <Link to="/contact" className="text-textPrimary hover:text-white transition-colors">
                        Contact us
                      </Link> */}
           
           {/* <div className="flex gap-4">
-            <a href="https://github.com/Gift-World" target="_blank" rel="noopener noreferrer" className="text-textPrimary hover:text-secondary transition-colors">
+            <a href="https://github.com/Gift-World" target="_blank" rel="noopener noreferrer" className="text-textPrimary hover:text-white transition-colors">
               <FaGithub size={20} />
             </a>
-            <a href="https://www.linkedin.com/in/charles-ang-ila-b83b2733a/" target="_blank" rel="noopener noreferrer" className="text-textPrimary hover:text-secondary transition-colors">
+            <a href="https://www.linkedin.com/in/charles-ang-ila-b83b2733a/" target="_blank" rel="noopener noreferrer" className="text-textPrimary hover:text-white transition-colors">
               <FaLinkedin size={20} />
             </a>
             
