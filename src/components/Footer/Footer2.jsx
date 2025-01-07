84622f3c2f803d9d592236be3eeda285925efc10
Charles <charlesgiftangila@gmail.com>
2025-01-07 12:23:15 +0300

    Commented out the CraftWise Academy link in Footer2 component

--- src/components/Footer/Footer2.jsx
+++ src/components/Footer/Footer2.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
   return (
     <footer className="bg-[#000b16] py-4 px-20 text-center">
   <div className="flex justify-between items-center">
-    <a href="#" className="text-white text-2xl font-bold">CraftWise Academy</a>
+    {/* <a href="#" className="text-white text-2xl font-bold">CraftWise Academy</a> */}
     <p className="text-gray-400">&copy; 2025 CraftWise Academy. All Rights Reserved</p>
     <ul className="flex space-x-4">
       <li className="flex justify-center items-center w-12 h-12 rounded-full border border-[#1aeeef] hover:bg-[#1aeeef] transition duration-500">
