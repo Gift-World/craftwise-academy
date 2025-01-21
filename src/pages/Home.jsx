8b99de68b75abef3c3d22c717d3709e335cc6c8e
Charles <charlesgiftangila@gmail.com>
2025-01-21 20:30:08 +0300

    `feat: update dependencies and add Google Analytics 4 tracking`

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,7 @@
-import { useEffect } from 'react';
+// import React from 'react'
 import { motion } from 'framer-motion';
-import ReactGA from 'react-ga4'; // or 'react-ga' if using Universal Analytics
 
-import Hero from '../components/Hero';
+import Hero from '../components/Hero'
 import Navbar from '../components/Navbar';
 import About from '../components/About';
 import WhatWeDo from '../components/WhatWeDo';
@@ -15,39 +14,39 @@ import WhyItMattersSection from '../components/WhyItMatters/WhyItMattersSection'
 import ImpactSection from '../components/Impact/ImpactSection';
 import PartnersSection from '../components/Partners/PartnersSection'; 
 import Footer from '../components/Footer/Footer';
+// import Header from '../components/Header';
 
 function Home() {
-    useEffect(() => {
-        const trackingID = 'GTM-PVCFKMZF'; // Replace with your actual GA Tracking ID
-        ReactGA.initialize(trackingID);
-        ReactGA.send({ hitType: 'pageview', page: window.location.pathname });
-    }, []);
+  return (
+    <div>
+      <Navbar />
+      <main>
+      {/* <Navbar /> */}
 
-    return (
-        <div>
-            <Navbar />
-            <main>
-                <motion.div
-                    initial={{ opacity: 0 }}
-                    animate={{ opacity: 1 }}
-                    transition={{ duration: 0.5 }}
-                >
-                    <Hero />
-                </motion.div>
-                <About />
-                <WhatWeDo />
-                <SolutionSection />
-                <OfficeLabsSection />
-                <ImmersiveSection />
-                <ExperienceSection />
-                <CraftersAssemblySection />
-                <WhyItMattersSection />
-                <ImpactSection />
-                <PartnersSection />
-                <Footer />
-            </main>
-        </div>
-    );
+
+<motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      
+      <Hero />
+    </motion.div>
+    <About />
+    <WhatWeDo />
+    <SolutionSection />
+    <OfficeLabsSection />
+    <ImmersiveSection/>
+    <ExperienceSection />
+    <CraftersAssemblySection />
+    <WhyItMattersSection />
+    <ImpactSection />
+    <PartnersSection />
+   
+    <Footer />
+    </main>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
