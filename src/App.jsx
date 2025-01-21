37e9b43af2dfe69c38c1e7b9ed9e5f61361b9716
Charles <charlesgiftangila@gmail.com>
2025-01-21 21:36:39 +0300

    `Removed Google Analytics initialization and tracking code`

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,25 @@
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
-import { useEffect } from 'react';
-import ReactGA from 'react-ga4';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Programs from './pages/Programs';
 import BenefitsSection from './components/Benefits/BenefitsSection';
 import ContactSection from './components/Contact/ContactSection';
 
-// Initialize Google Analytics with your Measurement ID
-ReactGA.initialize('G-YWHKQVT9EW'); // Replace with your actual Measurement ID
 
-// Component to track page views
-const TrackPageView = () => {
-  const location = useLocation();
-
-  useEffect(() => {
-    ReactGA.send({ hitType: 'pageview', page: location.pathname });
-  }, [location]);
-
-  return null;
-};
 
 function App() {
   return (
     <Router>
-      <TrackPageView />
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/programs" element={<Programs />} />
         <Route path="/benefits" element={<BenefitsSection />} />
-        <Route path="/contact" element={<ContactSection />} />
+        <Route path="/contact" element={ <ContactSection  />} />
+
+        
+
+
       </Routes>
     </Router>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ReactGA from 'react-ga4';
-ReactGA.initialize('G-YWHKQVT9EW'); // Replace with your Measurement ID
-ReactGA.send('pageview'); // Track the initial pageview
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
