5eed26bfbaa846ba6b533a3a3396691337e3e26a
Charles <charlesgiftangila@gmail.com>
2025-01-21 20:43:59 +0300

    `Update Google Analytics Measurement ID in App.jsx and main.jsx`

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import BenefitsSection from './components/Benefits/BenefitsSection';
 import ContactSection from './components/Contact/ContactSection';
 
 // Initialize Google Analytics with your Measurement ID
-ReactGA.initialize('GTM-TD2GJ9HV'); // Replace with your actual Measurement ID
+ReactGA.initialize('G-YWHKQVT9EW'); // Replace with your actual Measurement ID
 
 // Component to track page views
 const TrackPageView = () => {
