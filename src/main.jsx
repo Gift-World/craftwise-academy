c818d73698689aa00cccae4db62e6dc82a47b08c
Charles <charlesgiftangila@gmail.com>
2025-01-21 20:31:08 +0300

    `Update ReactGA initialization ID in main.jsx`

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ReactGA from 'react-ga4';
-ReactGA.initialize('G-XXXXXXXXXX'); // Replace with your Measurement ID
+ReactGA.initialize('GTM-TD2GJ9HV'); // Replace with your Measurement ID
 ReactGA.send('pageview'); // Track the initial pageview
 
 createRoot(document.getElementById('root')).render(
