48b25558bb6edd1100c90a4c81171b5035994958
Charles <charlesgiftangila@gmail.com>
2025-01-30 13:44:26 +0300

    refactor: Add @heroicons/react dependency and remove Programs link from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,9 +26,7 @@ const Navbar = () => {
             <Link to="/benefits"  className="font-montserrat font-medium text-[15px]  inline-flex items-center px-1 pt-1 text-gray-700 hover:text-orange-500 transition-colors focus:outline-none">
               Benefits
             </Link>
-            <Link to="/programs"  className=" font-montserrat font-medium text-[15px]  inline-flex items-center px-1 pt-1 text-gray-700 hover:text-orange-500 transition-colors focus:outline-none">
-              Programs
-            </Link>
+            .
             <Link to="/contact"  className=" font-montserrat font-medium text-[15px]  inline-flex items-center px-1 pt-1 text-gray-700 hover:text-orange-500 transition-colors focus:outline-none">
               Contact us
             </Link>
